aa7ac9474ee3193cd0f48ef5c81dcd4f0538d8df
Stanisław Paśkowski <stanislaw.paskowski@gmail.com>
2022-05-28 13:48:09 +0200

    Added TestAppDelegate

---
+++ iTunesPodcastsTests/TestAppDelegate.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+@objc(TestAppDelegate)
+class TestAppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        true
+    }
+}
